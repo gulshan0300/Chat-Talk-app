8c55a52e8cfaa200072e557958d813fc334c8c17
Gulshan <gulshansinghania0300@gmail.com>
2024-09-16 19:56:35 +0530

    chnage vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port:3000,
     proxy:{
       "/api":{
-        target:"http://localhost:5000",
+        target:"https://lets-talk-chat-app-ezm4.onrender.com",
       }
     }
   },
